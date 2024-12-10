1707040399407d4a6e1bc20d39fb1e8e3e31efff
Bibek Joshi <bebikjoshi118@gmail.com>
2024-12-10 17:34:54 +0545

    54

---
+++ day 54/new.js
@@ -0,0 +1,4 @@
+console.log("Hello world ");
+console.log("Hey i am console");
+console.log("Hey i am bibek ");
+console.log("Hey code is running ");
--- day 54/script.js
+++ day 54/script.js
@@ -1,2 +1,15 @@
 alert("Hello world");
 console.log("Hey i am console");
+console.log("Hey i am bibek ");
+console.log("Hey code is running ");
+var a = prompt("Enter your number ")
+var isTrue = confirm("Are you sure you to want to leave this page and leave this page and  blast your computer ")
+if(isTrue){
+    console.log(" computer is blasting ")
+}
+else{
+    console.log(
+        "computer is not blasting"
+    )
+}
+console.log("your number is + a =")
