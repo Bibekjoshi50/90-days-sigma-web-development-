fac405476627e4df377b1d39b8d7e400e71fcecf
Bibek Joshi <bebikjoshi118@gmail.com>
2024-12-12 11:55:10 +0545

    55
    JavaScript Variables, Data Types & Objects

---
+++ day 55/script.js
@@ -0,0 +1,21 @@
+console.log("hellow world ");
+var a  = 5;
+var b = 6;
+var c = bibek ;
+console.log(a + b+8);
+console.log(typeof a, typeof b , typeof c )
+
+let x = "Bibek JOshi ";
+let y = "22";
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r =null;
+console.log(x,y,z,p,q,r)
+console.log(typeof x,  typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+   "name" : "bibek jsohi",
+"job code" : 5600
+}
+console.log(o)
