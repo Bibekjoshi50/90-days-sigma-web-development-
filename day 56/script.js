f3ce8a56f0b4a64eb747efbb9e0d557c1b014936
Bibek Joshi <bebikjoshi118@gmail.com>
2024-12-14 10:25:12 +0545

    56

---
+++ day 56/script.js
@@ -0,0 +1,15 @@
+console.log("Hello world")
+let age = 45;
+let grace = 2;
+console.log(age + grace)
+console.log(age - grace)
+console.log(age /grace)
+console.log(age * grace)
+console.log(age ** grace)
+console.log(age % grace)
+if (age> 18){
+    console.log("you can drive");
+}
+else{
+    console.log("you cannot drive");
+}
