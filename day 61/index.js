7c5a4f211b5c221e1a8ac1400dbfe6448ddc6d3a
Bibek Joshi <bebikjoshi118@gmail.com>
2024-12-15 19:02:53 +0545

    61

---
+++ day 61/index.js
@@ -0,0 +1,20 @@
+let random = Math.random()
+let a = prompt("Enter first number ")
+let b = prompt("Enter second  number ")
+let c = prompt("Enter Operation")
+
+let obj = {
+    "+": "-",
+    "*": "+",
+    "-": "/",
+    "/": "**",
+}
+
+
+
+if (random > 0.1){
+
+}
+else{
+    c =obj[c] 
+}
