b2b9958b6dc2b804973670c675234fee92fddf71
Bibek Joshi <bebikjoshi118@gmail.com>
2024-12-15 18:32:16 +0545

    60
    JavaScript Strings

--- day 60/index.js
+++ day 60/index.js
@@ -5,3 +5,16 @@ console.log(a[1])
 console.log(a[2])
 console.log(a[3])
 console.log(a[4])
+
+let name  = "Bibek"
+let friend ="Rohan"
+console.log("his name is " + name + " and his friendas name is " + friend)
+
+let b = "shivam"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1, 5))
+console.log(b.replace("sh","77")) 
+console.log(b)
+console.log(friend)
