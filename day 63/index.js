fdf3c20c072742d25466b7356ccc9e8efe5edf7d
Bibek Joshi <bebikjoshi118@gmail.com>
2024-12-16 14:29:31 +0545

    63
    JavaScript Arrays

---
+++ day 63/index.js
@@ -0,0 +1,19 @@
+let arr = [ 1,2 , 3 , 4 ,5 ]
+console.log(arr);
+console.log(arr.length)
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+console.log(arr[3])
+console.log(arr[4])
+
+let a1 = [1,2,3]
+let a2 = [4,5,6]
+let a3 = [9,8,7]
+
+a1.concat(a2,a3)
+
+let a = [7,8,9]
+a.sort()
+let Numbers = [1,2,3,4,5]
+Numbers.splice(1,2)
---
+++ day 63/loops.js
@@ -0,0 +1,9 @@
+let a = [1, 93,5, 6, 88]
+a.forEach((value , index , arr)=>{
+    console.log(value,index, arr)
+})
+// let obj ={
+//     a:1;
+//     b:2;
+//     c:3;
+// }
