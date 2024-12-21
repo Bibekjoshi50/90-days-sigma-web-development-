35367ba6f66e2a0d1d5f6ba53e2a89d69ac65e70
Bibek Joshi <bebikjoshi118@gmail.com>
2024-12-21 18:16:50 +0545

    68

---
+++ day 68/script.js
@@ -0,0 +1,7 @@
+console.log("Bibek joshi");
+let bibekjoshi =document.getElementsByClassName("box");
+console.log(bibekjoshi);
+bibekjoshi[2].style.backgroundcolor = "red";
+document.getElementById("two").style.backgroundcolor = "red";
+document.querySelector(".box").style.backgroundcolor ="green";
+style.backgroundcolor = "green";
